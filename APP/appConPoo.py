397d6fcf1fbbc699d7112fa3f1926cc7ef82a294
SebastianTovar64 <129071813+SebastianTovar64@users.noreply.github.com>
2023-11-27 17:40:11 -0500

    pooActualizado
    POO

--- APP/appConPoo.py
+++ APP/appConPoo.py
@@ -1,4 +1,6 @@
 class Equipo:
+    equipos = {}  # Diccionario de clase para almacenar instancias de equipos.
+
     def __init__(self, equipoId, cargador, mouse):
         # Constructor de la clase Equipo, inicializa los atributos del equipo.
         self.equipoId = equipoId
@@ -6,17 +8,28 @@ class Equipo:
         self.mouse = mouse
         self.novedades = []  # Lista para almacenar novedades del equipo.
         self.ambientes = []  # Lista para almacenar ambientes del equipo.
+        Equipo.equipos[equipoId] = self  # Almacena la instancia en el diccionario de clase.
+
+    def crear_equipo():
+        # Metodo estático para crear instancias de equipos.
+        equipoId = input("Ingresa el ID del equipo: ")
+        if equipoId in Equipo.equipos:
+            print("El equipo ya existe.")
+            return
+        cargador = input("Ingresa el numero del cargador: ")
+        mouse = input("Ingresa el numero del mouse: ")
+        return Equipo(equipoId, cargador, mouse)
 
     def agregar_novedad(self, fecha, descripcion):
         # Metodo para agregar una nueva novedad al equipo.
         self.novedades.append({"fecha": fecha, "descripcion": descripcion})
 
     def mostrar_novedades(self):
-        # Método para mostrar las novedades del equipo.
+        # Metodo para mostrar las novedades del equipo.
         if self.novedades:
             print(f"Novedades del equipo con ID {self.equipoId}:")
             for novedad in self.novedades:
-                print(f"Fecha: {novedad['fecha']}, Descripción: {novedad['descripcion']}")
+                print(f"Fecha: {novedad['fecha']}, Descripcion: {novedad['descripcion']}")
         else:
             print("No hay novedades para este equipo.")
 
@@ -25,8 +38,8 @@ class Equipo:
         self.ambientes.append(ambiente)
 
     def mostrar_informacion(self):
-        # Metodo para mostrar la informacion completa del equipo.
-        print(f"Información del equipo con ID {self.equipoId}:")
+        # Metodo para mostrar la información completa del equipo.
+        print(f"Informacion del equipo con ID {self.equipoId}:")
         print(f"Cargador: {self.cargador}")
         print(f"Mouse: {self.mouse}")
         self.mostrar_novedades()
@@ -38,110 +51,93 @@ class Equipo:
             print("No hay ambientes asignados para este equipo.")
 
 
-equipos = {}  # Diccionario para almacenar todos los equipos.
-
 def agregar_equipo():
-    # Funcion para agregar un nuevo equipo al diccionario 'equipos'.
-    equipoId = input("Ingresa el ID del equipo: ")
-    if equipoId in equipos:
-        print("El equipo ya existe.")
-        return
-    cargador = input("Ingresa el número del cargador: ")
-    mouse = input("Ingresa el número del mouse: ")
-    equipos[equipoId] = Equipo(equipoId, cargador, mouse)
-    print("Equipo agregado con éxito.")
+    equipo = Equipo.crear_equipo()
+    if equipo:
+        print("Equipo agregado correctamente.")
+
 
 def agregar_novedad():
-    # Funcion para agregar una novedad a un equipo existente.
     equipoId = input("Ingresa el ID del equipo: ")
-    if equipoId in equipos:
-        fecha = input("Ingresa la fecha de la novedad: ")
-        descripcion = input("Ingresa una descripción de la novedad: ")
-        equipos[equipoId].agregar_novedad(fecha, descripcion)
-        print("Novedad agregada con éxito.")
-    else:
-        print("Equipo no encontrado.")
+    if equipoId not in Equipo.equipos:
+        print("El equipo no existe.")
+        return
+    fecha = input("Ingresa la fecha de la novedad: ")
+    descripcion = input("Ingresa la descripcion de la novedad: ")
+    Equipo.equipos[equipoId].agregar_novedad(fecha, descripcion)
+    print("Novedad agregada correctamente.")
 
-def buscar_equipo():
-    # Funcion para buscar un equipo por su ID y mostrar su informacion.
-    equipoId = input("Ingresa el ID del equipo que deseas buscar: ")
-    if equipoId in equipos:
-        equipos[equipoId].mostrar_informacion()
-    else:
-        print("Equipo no encontrado.")
 
 def mostrar_equipos_con_novedades():
-    # Funcion para mostrar la informacion de equipos que tienen novedades.
-    equipos_novedades = [equipoId for equipoId, equipo in equipos.items() if equipo.novedades]
-    if equipos_novedades:
+    equipos_con_novedades = [equipo for equipo in Equipo.equipos.values() if equipo.novedades]
+    if equipos_con_novedades:
         print("Equipos con novedades:")
-        for equipoId in equipos_novedades:
-            equipos[equipoId].mostrar_informacion()
-            print()
+        for equipo in equipos_con_novedades:
+            print(f"ID: {equipo.equipoId}, Cargador: {equipo.cargador}, Mouse: {equipo.mouse}")
     else:
         print("No hay equipos con novedades.")
 
+
 def eliminar_equipo():
-    # Funcion para eliminar un equipo del diccionario 'equipos'.
-    equipoId = input("Ingresa el ID del equipo que deseas eliminar: ")
-    if equipoId in equipos:
-        del equipos[equipoId]
-        print("Equipo eliminado con éxito.")
-    else:
-        print("Equipo no encontrado.")
+    equipoId = input("Ingresa el ID del equipo a eliminar: ")
+    if equipoId not in Equipo.equipos:
+        print("El equipo no existe.")
+        return
+    del Equipo.equipos[equipoId]
+    print("Equipo eliminado correctamente.")
+
 
 def modificar_equipo():
-    # Funcion para modificar ciertos atributos de un equipo existente.
-    equipoId = input("Ingresa el ID del equipo que deseas modificar: ")
-    if equipoId in equipos:
-        equipo = equipos[equipoId]
-        campo = input("Ingresa el campo que deseas modificar (cargador, mouse): ")
-        nuevoValor = input("Ingresa el nuevo valor del campo: ")
-        setattr(equipo, campo, nuevoValor)
-        print("Equipo modificado con éxito.")
-    else:
-        print("Equipo no encontrado.")
+    equipoId = input("Ingresa el ID del equipo a modificar: ")
+    if equipoId not in Equipo.equipos:
+        print("El equipo no existe.")
+        return
+    cargador = input("Ingresa el nuevo numero del cargador: ")
+    mouse = input("Ingresa el nuevo numero del mouse: ")
+    Equipo.equipos[equipoId].cargador = cargador
+    Equipo.equipos[equipoId].mouse = mouse
+    print("Equipo modificado correctamente.")
+
 
 def agregar_ambiente():
-    # Funcion para agregar un ambiente a la lista de ambientes de un equipo.
-    equipoId = input("Ingresa el ID del equipo al que deseas agregar un ambiente: ")
-    if equipoId in equipos:
-        equipo = equipos[equipoId]
-        ambiente = input("Ingresa el ambiente que deseas agregar: ")
-        equipo.agregar_ambiente(ambiente)
-        print("Ambiente agregado con éxito.")
-    else:
-        print("Equipo no encontrado.")
+    equipoId = input("Ingresa el ID del equipo: ")
+    if equipoId not in Equipo.equipos:
+        print("El equipo no existe.")
+        return
+    ambiente = input("Ingresa el ambiente a agregar: ")
+    Equipo.equipos[equipoId].agregar_ambiente(ambiente)
+    print("Ambiente agregado correctamente.")
 
-# Bucle principal del programa
-while True:
-    print("\nOpciones:")
-    print("1. Agregar equipo")
-    print("2. Agregar novedad")
-    print("3. Buscar equipo por ID")
-    print("4. Mostrar equipos con novedades")
-    print("5. Eliminar Equipo")
-    print("6. Modificar equipo")
-    print("7. Agregar a qué ambiente pertenece")
-    print("8. Salir")
 
-    opcion = input("Selecciona una opción: ")
+def mostrar_menu():
+    while True:
+        print("\n--- Menu ---")
+        print("1. Agregar un nuevo equipo")
+        print("2. Agregar una nueva novedad")
+        print("3. Mostrar equipos con novedades")
+        print("4. Eliminar un equipo")
+        print("5. Modificar un equipo")
+        print("6. Agregar un ambiente a un equipo")
+        print("7. Salir")
+        
+        opcion = input("Selecciona una opción: ")
 
-    if opcion == "1":
-        agregar_equipo()
-    elif opcion == "2":
-        agregar_novedad()
-    elif opcion == "3":
-        buscar_equipo()
-    elif opcion == "4":
-        mostrar_equipos_con_novedades()
-    elif opcion == "5":
-        eliminar_equipo()
-    elif opcion == "6":
-        modificar_equipo()
-    elif opcion == "7":
-        agregar_ambiente()
-    elif opcion == "8":
-        break
-    else:
-        print("Opción no válida. Por favor, selecciona una opción válida.")
+        if opcion == "1":
+            agregar_equipo()
+        elif opcion == "2":
+            agregar_novedad()
+        elif opcion == "3":
+            mostrar_equipos_con_novedades()
+        elif opcion == "4":
+            eliminar_equipo()
+        elif opcion == "5":
+            modificar_equipo()
+        elif opcion == "6":
+            agregar_ambiente()
+        elif opcion == "7":
+            break
+        else:
+            print("Opcion invalida. Por favor, selecciona una opcion valida.")
+
+
+mostrar_menu()
